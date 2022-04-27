e1d28b4239642d831830aa3e7cd93e61931d2159
at-impacta <atus.silva@aluno.faculdadeimpacta.com.br>
2022-04-27 19:44:01 -0300

    Incluir Elastic IP para o Jenkins Server

--- main.tf
+++ main.tf
@@ -45,3 +45,17 @@ module "ec2_instance" {
     OwnerSRE = "Valfenda"
   }
 }
+
+resource "aws_eip" "jenkins-ip" {
+  instance = module.ec2_instance.id
+  vpc = true
+
+  tags = {
+    Name = "jenkins-eip"
+    Terraform = "true"
+    Environment = "Production"
+    CC = "19931204"
+    OwnerSquad = "Osaka"
+    OwnerSRE = "Valfenda"
+  }
+}
